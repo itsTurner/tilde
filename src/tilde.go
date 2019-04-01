3f30bbf2a334d670051fadb8a013875a9c189d24
Tavin Turner <camelinatardis@gmail.com>
2019-03-31 20:56:45 -0600

    Initialize Tilde class and running functions

--- src/tilde.go
+++ src/tilde.go
@@ -3,7 +3,21 @@ package main
 import(
   "fmt"
   "os"
+  "io/ioutil"
+  "bufio"
 )
+
+type Tilde struct {
+  HadError bool
+}
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+var tilde Tilde
 
 func main() {
   args := os.Args
@@ -18,5 +32,43 @@ func main() {
     tilde.RunFile(argv[0])
   } else {
     tilde.RunPrompt()
+  }
+}
+
+
+func (t Tilde) Error(line int, message string) {
+  t.Report(line, "", message)
+}
+
+func (t Tilde) Report(line int, where string, message string) {
+  fmt.Println("[Line %i] Error%s: %s", line, where, message)
+  t.HadError = true
+}
+
+func (t Tilde) RunFile(path string) {
+  dat, err := ioutil.ReadFile(path)
+  check(err)
+  data := string(dat[:])
+  t.Run(data)
+
+  if t.HadError os.Exit(65)
+}
+
+func (t Tilde) RunPrompt() {
+  reader := bufio.NewReader(os.Stdin)
+  for {
+    fmt.Print("> ")
+    text, _ := reader.ReadString('\n')
+    t.Run(text)
+    t.HadError = false
+  }
+}
+
+func (t Tilde) Run(source string) {
+  scanner := Scanner{source}
+  tokens := scanner.ScanTokens()
+
+  for _, token := range(tokens) {
+    fmt.Println(token)
   }
 }
