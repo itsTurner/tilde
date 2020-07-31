7cc6664f3a1a5ad6d45682466aa2ffff545978a1
Tavin Turner <camelinatardis@gmail.com>
2020-07-31 02:17:22 -0600

    Instruction type

--- chunk.go
+++ chunk.go
@@ -1,15 +1,18 @@
 package main
 
+// Instruction is a byte representing bytecode instruction
+type Instruction byte
+
 // Chunk represents section of bytecode
 type Chunk struct {
-	code []byte
+	code []Instruction
 }
 
-func (c Chunk) Write(b byte) {
+func (c Chunk) Write(b Instruction) {
 	c.code = append(c.code, b)
 }
 
 // Opcode enumeration
 const (
-	OpReturn uint8 = iota
+	OpReturn Instruction = iota
 )
--- chunk_test.go
+++ chunk_test.go
@@ -8,7 +8,7 @@ import (
 func TestChunkWrite(t *testing.T) {
 	chunk := Chunk{}
 	chunk.Write(OpReturn)
-	expected := Chunk{code: []byte{OpReturn}}
+	expected := Chunk{code: []Instruction{OpReturn}}
 	if reflect.DeepEqual(chunk.code, expected.code) {
 		t.Fatalf("Chunk.Write() does not append to Chunk code")
 	}
